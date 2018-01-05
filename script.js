437bf12949cb013d0a3e586c3c6c72fd4143aa5b
Michael <michd.weaver@gmail.com>
2018-01-04 21:47:17 -0500

    added name elements to js

--- script.js
+++ script.js
@@ -19,9 +19,9 @@ $(document).ready(function() {
     $('.name').css('display', 'block');
     $('.intro').text("Click to go somewhere else.");
 
-    var prefx = ['Aber', 'Ash', 'Ast', 'Avon', 'Axe', 'Bally', 'Beck', 'Bex', 'Bir', 'Brad', 'Cul', 'Drum', 'Dun', 'Foss', 'Folk', 'Inver', 'Kil', 'Kirk', 'Lang', 'Lin', 'Liver', 'Lon', 'Nor', 'Shep', 'Sud', 'Tre', 'Win'];
-    var root = ['', '', 'bourne', 'den', 'don', 'firth', 'ford', 'gate', 'holm', 'ing', 'king', 'leigh', 'ley', 'mere', 'more', 'ock', 'stow', 'wold', 'wood', 'ton', '', ''];
-    var sufx = ['', '', 'berry', 'borough', 'bury', 'by', 'caster', 'castle', 'cester', 'chapel', 'dale', 'field', 'ham', 'minster', 'mouth', 'ness', 'pool', 'shire', 'sted', 'ster', 'ton', 'town', 'wall', 'wick', 'worth', ' Abbey', ' Castle', ' Downs', ' Glen', ' Green', ' Prior', ' Village', '-on-the-Water', '', ''];
+    var prefx = ['Aber', 'Ash', 'Ast', 'Avon', 'Axe', 'Bally', 'Beck', 'Bex', 'Bir', 'Brad', 'Caer', 'Cul', 'Drum', 'Dun', 'Foss', 'Folk', 'Inver', 'Kil', 'Kirk', 'Lang', 'Lin', 'Liver', 'Lon', 'Moss', 'Nor', 'Ruther', 'Shep', 'Sud', 'Tilly', 'Tre', 'Win'];
+    var root = ['', '', 'bourne', 'den', 'don', 'firth', 'gill', 'ford', 'gate', 'holm', 'ing', 'king', 'leigh', 'ley', 'lin', 'mere', 'more', 'ock', 'stow', 'wold', 'wood', 'ton', '', ''];
+    var sufx = ['', '', 'barrow', 'berry', 'burgh', 'borough', 'bury', 'by', 'caster', 'castle', 'cester', 'chapel', 'dale', 'field', 'ham', 'minster', 'mouth', 'ness', 'pool', 'shire', 'sted', 'ster', 'ton', 'town', 'wall', 'wick', 'worth', ' Abbey', ' Castle', ' Downs', ' Glen', ' Green', ' Hay', ' Prior', ' Village', '-on-the-Water', '', ''];
 
     var a = ~~(Math.random() * prefx.length);
     var b = ~~(Math.random() * root.length);
@@ -36,9 +36,12 @@ $(document).ready(function() {
       $('.name').click();
     else if (root[b] === 'ton' && sufx[c] === 'town')
       $('.name').click();
+    else if (prefx[a] === 'Lin' && root[b] === 'lin')
+      $('.name').click();
     else if (fullName.length > 12 && fullName.indexOf(' ') === -1)
       $('.name').click();
     else {
+      // assemble and display name
       for (var i = 0; i < fullName.length - 2; i++) {
         if (fullName[i] === fullName[i + 1] && fullName[i] === fullName[i + 2])
           $('.name').click();
